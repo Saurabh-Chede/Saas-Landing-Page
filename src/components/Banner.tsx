88baaf6c0822c93fa01e59e4afba96dc3a6d6a7f
Saurabh-Chede <saurabhchede21@gmail.com>
2025-05-15 20:17:57 +0530

    landing page is ready

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,7 @@
 export const Banner = () => {
   return (
     <div className="flex w-full text-black text-base py-2 justify-center bg-[linear-gradient(to_right,#fcf403,#1cfc03,#f8fc03,#fc0339,#6f03fc)]">
-      <div className="">Get the Course.</div>
+      <div className="">Get the kit.</div>
     </div>
   );
 };
